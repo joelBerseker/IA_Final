ddc2fc95357830bfa24792d81a5737b3793e38b6
joelBerseker <jatamaria@unsa.edu.pe>
2020-08-06 16:38:49 -0500

    Leer Archivos CSV

---
+++ inicio.py
@@ -0,0 +1,7 @@
+import csv
+ 
+with open('dataset.csv') as File:
+    reader = csv.reader(File, delimiter=',', quotechar=',',
+                        quoting=csv.QUOTE_MINIMAL)
+    for row in reader:
+        print(row)
